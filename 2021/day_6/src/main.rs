35b5826fed2a4920ff598a44f6ae2bf01e83cbca
MatiF100 <mateuszfesz@wp.pl>
2021-12-06 22:41:09 +0100

    Added support for bigger numbers (realistically up to tenths, maybe hundreds of millions)

--- 2021/day_6/src/main.rs
+++ 2021/day_6/src/main.rs
@@ -1,3 +1,5 @@
+use num::bigint::{BigInt, BigUint, ToBigInt, ToBigUint};
+use num::cast::ToPrimitive;
 fn main() {
     let input = if let Ok(s) = std::fs::read_to_string("input.txt") {
         s
@@ -11,8 +13,9 @@ fn main() {
 
     println!("Part 2: {}", calc(&input, 80));
     println!("Part 2: {}", calc(&input, 256));
-    println!("The following can crash for your input data as i128 is a bit limited. Please adjust the limit if it does so");
-    println!("bench: {}", calc(&input, 943));
+    println!("The following can take A LOT of time, since value passed is 10 million. If you wish, you can adjust it as necessary");
+    let input = input.iter().map(|x| x.to_bigint().unwrap()).collect::<Vec<_>>();
+    println!("bench: {}", calc_bi(&input, 10.to_biguint().unwrap().pow(7)));
 }
 
 fn calc(init: &Vec<i64>, count: usize) -> i128{
@@ -33,4 +36,40 @@ fn calc(init: &Vec<i64>, count: usize) -> i128{
         
     }
     states.iter().sum::<i128>()
+}
+
+fn calc_bi(init: &Vec<BigInt>, count: BigUint) -> BigInt {
+    //7 days is a cycle
+    //1 day is a phase
+    let mut states: [BigInt; 9] = [
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+        0i32.to_bigint().unwrap(),
+    ];
+
+    init.iter()
+        .for_each(|x| states[(*x).to_usize().unwrap()] += 1);
+    let mut cnt = 0.to_biguint().unwrap();
+    while cnt < count{
+    //for _ in 0..count {
+        states = [
+            states[1].clone(),
+            states[2].clone(),
+            states[3].clone(),
+            states[4].clone(),
+            states[5].clone(),
+            states[6].clone(),
+            states[7].clone() + states[0].clone(),
+            states[8].clone(),
+            states[0].clone(),
+        ];
+        cnt = cnt + 1.to_biguint().unwrap();
+    }
+    states.iter().sum()
 }
